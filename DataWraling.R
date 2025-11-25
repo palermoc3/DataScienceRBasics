ce2b2c0ec4bc0fdf2fc20ff7ee2999d773d84e51
Pedro <pedropalermo97@gmail.com>
2025-11-25 15:35:15 -0300

    Sorting data tables

--- DataWraling.R
+++ DataWraling.R
@@ -47,3 +47,27 @@ murders %>% group_by(region)
 murders %>% 
   group_by(region) %>%
   summarize(median = median(rate))
+
+# order the states by population size
+murders %>% arrange(population) %>% head()
+
+# order the states by murder rate - the default is ascending order
+murders %>% arrange(rate) %>% head()
+
+# order the states by murder rate in descending order
+murders %>% arrange(desc(rate)) %>% head()
+
+# order the states by region and then by murder rate within region
+murders %>% arrange(region, rate) %>% head()
+
+# return the top 10 states by murder rate
+murders %>% top_n(10, rate)
+
+# return the top 10 states ranked by murder rate, sorted by murder rate
+murders %>% arrange(desc(rate)) %>% top_n(10)
+
+# return 4 regions ranked by murder rate, sorted by murder rate
+a <- murders %>% 
+  group_by(region) %>% arrange(desc(rate)) %>% head()
+a
+
